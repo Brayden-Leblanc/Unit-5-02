975300da7657a8835f42292542989937cbbfdf63
BraydenLeblanc <brayden.leblanc@ocsbstudent.ca>
2021-02-10 17:58:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function buttonOn() {
+  document.getElementById("textExample").innerHTML = "This is the new text!"
+}
+
+function timeChange () {
+  document.getElementById("picture").src = "images/night.jpg"
+  document.getElementById("background").style.backgroundColor = "MidnightBlue"
+}
+
+function disappearing() {
+  document.getElementById("disappear").style.display = "none";
+}
