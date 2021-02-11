9265fe5efae418109fbf11942533dac398214df7
BraydenLeblanc <brayden.leblanc@ocsbstudent.ca>
2021-02-11 15:50:28 +0000

    fixed javascript

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function buttonon() {
+function buttonon () {
   document.getElementById('textexample').innerHTML = 'This is the new text!'
   // This tells Javascript to change the id's HTML to the text provided on the press of the button.
 }
 
-function timechange() {
+function timechange () {
   document.getElementById('picture').src = 'images/night.jpg'
   // This tells Javascript to change the id's image to the image provided on the press of the button.
   document.getElementById('background').style.backgroundColor = 'MidnightBlue'
-  // This tells Javascript 
+  // This tells Javascript
 }
 
-function disappearing() {
+function disappearing () {
   document.getElementById('disappear').style.display = 'none'
 }
