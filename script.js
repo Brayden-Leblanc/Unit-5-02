8b94c818537a72e72c07fd1b37627becb3b3a4d6
BraydenLeblanc <brayden.leblanc@ocsbstudent.ca>
2021-02-11 15:44:23 +0000

    fixed javascript errors

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function buttonon() {
-  document.getElementById("textexample").innerHTML = "This is the new text!"
+  document.getElementById('textexample').innerHTML = 'This is the new text!'
   // This tells Javascript to change the id's HTML to the text provided on the press of the button.
 }
 
-function timechange () {
-  document.getElementById("picture").src = "images/night.jpg"
+function timechange() {
+  document.getElementById('picture').src = 'images/night.jpg'
   // This tells Javascript to change the id's image to the image provided on the press of the button.
-  document.getElementById("background").style.backgroundColor = "MidnightBlue"
+  document.getElementById('background').style.backgroundColor = 'MidnightBlue'
   // This tells Javascript 
 }
 
 function disappearing() {
-  document.getElementById("disappear").style.display = "none";
+  document.getElementById('disappear').style.display = 'none'
 }
