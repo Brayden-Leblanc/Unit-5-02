1ec734986101bad806d886e9339d9c606d8605b4
BraydenLeblanc <brayden.leblanc@ocsbstudent.ca>
2021-02-11 15:24:10 +0000

    fixed css and html errors

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function buttonon() {
   document.getElementById("textexample").innerHTML = "This is the new text!"
+  // This tells Javascript to change the id's HTML to the text provided on the press of the button.
 }
 
-function timeChange () {
+function timechange () {
   document.getElementById("picture").src = "images/night.jpg"
+  // This tells Javascript to change the id's image to the image provided on the press of the button.
   document.getElementById("background").style.backgroundColor = "MidnightBlue"
+  // This tells Javascript 
 }
 
 function disappearing() {
